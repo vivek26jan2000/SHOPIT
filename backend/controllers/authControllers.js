1ef7058cc5cbf428cd7d96a6b2cbc978ce0b1b90
vivek26jan2000 <v26jan2000@gmail.com>
2025-12-12 15:32:05 +0530

    add Users routes

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -54,3 +54,103 @@ export const logout = catchAsyncErrors(async (req, res, next) => {
     message: "Logged out",
   });
 });
+
+// current login user ==> api/v1/me
+export const getUserProfile = catchAsyncErrors(async (req, res, next) => {
+  const user = await User.findById(req?.user?._id);
+
+  res.status(200).json({
+    user,
+  });
+});
+
+// update password ==> api/v1/update/password
+export const updatePassword = catchAsyncErrors(async (req, res, next) => {
+  const user = await User.findById(req?.user?._id).select("+password");
+
+  const isPasswordMatched = await user.isPasswordCorrect(req.body.oldPassword);
+
+  if (!isPasswordMatched) {
+    return next(new ErrorHandler("OldPassword is incorrect", 400));
+  }
+
+  user.password = req.body.password;
+  await user.save();
+
+  res.status(200).json({
+    success: true,
+  });
+});
+
+// update profile ==> api/v1/me/update
+export const updateProfile = catchAsyncErrors(async (req, res, next) => {
+  const newData = {
+    name: req.body.name,
+    email: req.body.email,
+  };
+
+  const user = await User.findByIdAndUpdate(req?.user?._id, newData, {
+    new: true,
+  });
+
+  res.status(200).json({
+    user,
+  });
+});
+
+// get all users (admin) ==> api/v1/admin/users
+export const allUsers = catchAsyncErrors(async (req, res, next) => {
+  const users = await User.find();
+  res.status(200).json({
+    users,
+  });
+});
+
+// get user details (admin) ==> api/v1/admin/users/:id
+export const getUserDetails = catchAsyncErrors(async (req, res, next) => {
+  const user = await User.findById(req.params.id);
+
+  if (!user) {
+    return next(
+      new ErrorHandler(`User is not found with id:${req.params.id}`, 404)
+    );
+  }
+
+  res.status(200).json({
+    user,
+  });
+});
+
+//  update user (admin) ==> api/v1/admin/users/:id
+export const updateUser = catchAsyncErrors(async (req, res, next) => {
+  const newData = {
+    name: req.body.name,
+    email: req.body.email,
+    role: req.body.role,
+  };
+
+  const user = await User.findByIdAndUpdate(req?.user?._id, newData, {
+    new: true,
+  });
+
+  res.status(200).json({
+    user,
+  });
+});
+
+// delete user (admin) ==> api/v1/admin/users/:id
+export const deleteUser = catchAsyncErrors(async (req, res, next) => {
+  const user = await User.findById(req.params.id);
+
+  if (!user) {
+    return next(
+      new ErrorHandler(`User is not found with id:${req.params.id}`, 404)
+    );
+  }
+
+  await user.deleteOne();
+
+  res.status(200).json({
+    success: true,
+  });
+});
--- backend/routes/auths.js
+++ backend/routes/auths.js
@@ -3,12 +3,34 @@ import {
   registerUser,
   loginUser,
   logout,
+  getUserProfile,
+  updatePassword,
+  updateProfile,
+  allUsers,
+  getUserDetails,
+  updateUser,
+  deleteUser,
 } from "../controllers/authControllers.js";
+
+import { isAuthenticatedUser, authorizeRoles } from "../middlewares/auth.js";
 
 const router = express.Router();
 
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
 router.route("/logout").get(logout);
+
+router.route("/me").get(isAuthenticatedUser, getUserProfile);
+router.route("/me/update").put(isAuthenticatedUser, updateProfile);
+router.route("/update/password").post(isAuthenticatedUser, updatePassword);
+
+router
+  .route("/admin/users")
+  .get(isAuthenticatedUser, authorizeRoles("admin"), allUsers);
+router
+  .route("/admin/users/:id")
+  .get(isAuthenticatedUser, authorizeRoles("admin"), getUserDetails)
+  .put(isAuthenticatedUser, authorizeRoles("admin"), updateUser)
+  .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteUser);
 
 export default router;
